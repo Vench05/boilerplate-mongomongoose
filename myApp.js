782f8198854fd7dcbb223d798af9a1b89b4bc88a
Vench05 <ralfvenchbillones@gmail.com>
2021-05-30 22:02:21 +0800

    Connect to mongoDB

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
 
+const MONGO_URI = process.env.MONGO_URI
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 let Person;
 
